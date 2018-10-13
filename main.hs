eae40f43cbdd7c7c98c71a8c7c8985f26fb9cb73
uf <elyahove@student.unit.ua>
2018-10-13 20:21:29 +0300

    Add InputT input.

--- main.hs
+++ main.hs
@@ -9,12 +9,14 @@ import Parsers.GenericParsers
 import Operations.ComputorState
 
 import Control.Monad.State.Strict
+import System.Console.Haskeline
 import System.IO (hFlush, hIsEOF, stdout, stdin)
 import System.Exit (exitSuccess)
 
 computorParser :: Parser ComputorCommand
 computorParser = between sc eof parseComputorCommand
 
+{-
 interactiveConsole :: ComputorStateT IO ()
 interactiveConsole = do
   i <- liftIO promptInput
@@ -32,6 +34,23 @@ interactiveConsole = do
         exitSuccess
       else
         getLine
+-}
+
+
+interactiveConsole' :: InputT (ComputorStateT IO) ()
+interactiveConsole' = do
+  input <- getInputLine "$> "
+  case input of
+    Nothing -> lift . liftIO $ exitSuccess
+    Just i -> do
+                case parse computorParser "" i of
+                  Left err -> outputStr . errorBundlePretty $ err
+                  Right CNothing -> interactiveConsole'
+                  Right cmd -> lift $ executeCommand cmd
+                interactiveConsole'
 
 main :: IO ()
-main = evalStateT interactiveConsole initialState
+main = evalStateT (runInputT defaultSettings inputTRoutine) initialState
+  where
+    inputTRoutine = withInterrupt . (handle interruptHandler) $ interactiveConsole'
+    interruptHandler Interrupt = lift . liftIO $ exitSuccess
